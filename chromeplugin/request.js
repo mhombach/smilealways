dcbed82c30bbab0b0362f8b4900962f85e60bfce
Martin Hombach <martin.hombach@gmail.com>
2022-01-06 00:11:33 +0100

    Small refactor/renamings without any functionality change (hopefully). Untested.

--- chromeplugin/request.js
+++ chromeplugin/request.js
@@ -1,87 +1,89 @@
 /************************ REDIRECT CODE ***********************/
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
     return detectRedirect(details);
 }, {
-    urls : ["<all_urls>"],
-    types: ["main_frame","sub_frame"]
+    urls: ["<all_urls>"],
+    types: ["main_frame", "sub_frame"]
 }, ["blocking"]);
 
+function getAmazonUrlAndCountry(domain) {
+    if (domain.includes("amazon.de")) {
+        return { amazonurl: "www.amazon.de", country: "de" };
+    } else if (domain.includes("amazon.co.uk")) {
+        return { amazonurl: "www.amazon.co.uk", country: "uk" };
+    }
+    return { amazonurl: "www.amazon.com", country: "com" };
+}
 
-function detectRedirect(details) {
-    var url = details.url;
+function detectRedirect({ url }) {
+    const { url } = details;
 
     if (url == null) {
         return;
     }
 
-    var domain = url_domain(url);
-    var amazonurl = "www.amazon.com";
-    var country = "com";
-    if (domain.includes("amazon.de")) {
-    	amazonurl = "www.amazon.de";
-    	country = "de";
-    } else if (domain.includes("amazon.co.uk")) {
-      amazonurl = "www.amazon.co.uk";
-      country = "uk";
-    }
-
-    var https = "https://";
     // ignore links with these strings in them
-    var filter = "(sa-no-redirect=)"
-               + "|(redirect=true)"
-               + "|(redirect.html)"
-               + "|(r.html)"
-               + "|(f.html)"
-               + "|(/dmusic/cloudplayer)"
-               + "|(/photos)"
-               + "|(/wishlist)"
-               + "|(/clouddrive)"
-               + "|(/ap/)"
-               + "|(aws.amazon.)"
-               + "|(read.amazon.)"
-               + "|(login.amazon.)"
-               + "|(payments.amazon.)"
-               + "|(http://)"; //all Amazon pages now redirect to HTTPS, also fixes conflict with HTTPS Everywhere extension
+    const filter = "(sa-no-redirect=)"
+        + "|(redirect=true)"
+        + "|(redirect.html)"
+        + "|(r.html)"
+        + "|(f.html)"
+        + "|(/dmusic/cloudplayer)"
+        + "|(/photos)"
+        + "|(/wishlist)"
+        + "|(/clouddrive)"
+        + "|(/ap/)"
+        + "|(aws.amazon.)"
+        + "|(read.amazon.)"
+        + "|(login.amazon.)"
+        + "|(payments.amazon.)"
+        + "|(http://)"; //all Amazon pages now redirect to HTTPS, also fixes conflict with HTTPS Everywhere extension
 
     // Don't try and redirect pages that are in our filter
     if (url.match(filter) != null) {
         return;
     }
 
-    return redirectToSmile(https, amazonurl, url, country);
+    const { amazonurl, country } = getAmazonUrlAndCountry(url_domain(url));
+    return redirectToSmile("https://", amazonurl, url, country);
 }
 
 function redirectToSmile(scheme, amazonurl, url, country) {
-    var smileurl = "smile.amazon.com";
-    if (country === "de") {
-    	smileurl = "smile.amazon.de";
-    } else if (country === "uk") {
-      smileurl = "smile.amazon.co.uk";
-    }
+    const smileurl = getSmileUrlByCountry(country);
     return {
         // redirect to amazon smile append the rest of the url
-        redirectUrl : scheme + smileurl + getRelativeRedirectUrl(amazonurl, url)
+        redirectUrl: scheme + smileurl + getRelativeRedirectUrl(amazonurl, url)
     };
 }
 
+function getSmileUrlByCountry(country) {
+    switch (country) {
+        case "de":
+            return "smile.amazon.de";
+        case "uk":
+            return "smile.amazon.co.uk";
+        default:
+            return "smile.amazon.com";
+    }
+}
+
 function getRelativeRedirectUrl(amazonurl, url) {
-    var relativeUrl = url.split(amazonurl)[1];
-    var noRedirectIndicator = "sa-no-redirect=1";
-    var paramStart = "?";
-    var paramStartRegex = "\\" + paramStart;
-    var newurl = null;
+    const relativeUrl = url.split(amazonurl)[1];
+    const noRedirectIndicator = "sa-no-redirect=1";
+    const paramStart = "?";
+    const paramStartRegex = "\\" + paramStart;
 
     // check to see if there are already GET variables in the url
     if (relativeUrl.match(paramStartRegex) != null) {
-        newurl = relativeUrl + "&" + noRedirectIndicator;
+        return relativeUrl + "&" + noRedirectIndicator;
     } else {
-        newurl = relativeUrl + paramStart + noRedirectIndicator;
+        return relativeUrl + paramStart + noRedirectIndicator;
     }
-    return newurl;
+    return null;
 }
 
 function url_domain(data) {
-  var    a      = document.createElement('a');
-         a.href = data;
-  return a.hostname;
+    const a = document.createElement('a');
+    a.href = data;
+    return a.hostname;
 }
